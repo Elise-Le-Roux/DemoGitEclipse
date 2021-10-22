32ac42ec02ae6e7eb2c465a4d94d55b11f99181f
Elise <elise.lrdb@gmail.com>
2021-10-22 16:14:06 +0200

    this time we throw THE fish

---
+++ td3/src/td3/poisson.java
@@ -0,0 +1,11 @@
+package td3;
+
+import java.io.*;
+
+public class poisson {
+	public static void main(String[] args)
+    {
+		System.out.println("><()()()°>");
+		System.out.println("Look how I throw a fish , be like : (/^3^)/><()()()°>");
+    }
+}
